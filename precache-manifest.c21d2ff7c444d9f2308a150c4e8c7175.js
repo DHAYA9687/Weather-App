5be18b1c94742497ffc14193aa080125f7e79cad
öpenwork1 <sekarsekar5017@gmail.com>
2024-01-25 15:26:30 +0530

    Updates

---
+++ precache-manifest.c21d2ff7c444d9f2308a150c4e8c7175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4306ff48706f2acd8955b22630c553f0",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "73480f90155c32ae449a",
+    "url": "/Weather-App/static/css/main.735a45eb.chunk.css"
+  },
+  {
+    "revision": "0630b6f5646a28404c19",
+    "url": "/Weather-App/static/js/2.c5b298f7.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/Weather-App/static/js/2.c5b298f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73480f90155c32ae449a",
+    "url": "/Weather-App/static/js/main.a22f6a8b.chunk.js"
+  },
+  {
+    "revision": "db08d7e2706b583609bf",
+    "url": "/Weather-App/static/js/runtime-main.ef16a3a0.js"
+  }
+]);
